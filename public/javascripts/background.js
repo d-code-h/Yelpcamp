65f7a5aba09a08fef407e3efe5072cb808556eec
Habeeb David <david@localhost.localdomain>
2020-11-16 13:39:00 +0100

    set new backgrounds

--- public/javascripts/background.js
+++ public/javascripts/background.js
@@ -1,14 +1,14 @@
 var counter = 0;
 function changeBG(){
     var images = [
-        "https://images.unsplash.com/photo-1580638961081-3b414bc0328e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        "https://images.unsplash.com/photo-1550988547-bb59938650fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        "https://unsplash.com/photos/UInXoxvppA8",
-        "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        "https://images.unsplash.com/photo-1535576434247-e0f50b766399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        "https://images.unsplash.com/photo-1598128251172-b28ac1a9f63c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        "https://images.unsplash.com/photo-1581526065482-58bad358364b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        "https://images.unsplash.com/photo-1599177869610-cbd40b3fa28c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+        "url('https://images.unsplash.com/photo-1580638961081-3b414bc0328e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')",
+        "url('https://images.unsplash.com/photo-1550988547-bb59938650fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')",
+        "url('https://unsplash.com/photos/UInXoxvppA8')",
+        "url('https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')",
+        "url('https://images.unsplash.com/photo-1535576434247-e0f50b766399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')",
+        "url('https://images.unsplash.com/photo-1598128251172-b28ac1a9f63c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')",
+        "url('https://images.unsplash.com/photo-1581526065482-58bad358364b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')",
+        "url('https://images.unsplash.com/photo-1599177869610-cbd40b3fa28c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')"
       ]
     
     if(counter === images.length) counter = 0;
@@ -18,5 +18,3 @@ function changeBG(){
 }
   
   setInterval(changeBG, 2000);
-
-
